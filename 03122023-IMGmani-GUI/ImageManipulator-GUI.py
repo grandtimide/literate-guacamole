e5745026a371d338853371153efd9aa72a2009ff
awesomesauce <76840244+grandtimide@users.noreply.github.com>
2023-12-04 07:39:51 +0100

    Update up_quality_imager.py

--- 03122023-IMGmani-GUI/ImageManipulator-GUI.py
+++ 03122023-IMGmani-GUI/ImageManipulator-GUI.py
@@ -1,58 +1,25 @@
-from PIL import Image, ImageTk
-import tkinter as tk
-from tkinter import filedialog
+from tkinter import Tk, filedialog
+from PIL import Image, ImageEnhance
 
-def flip_horizontal(img):
-    return img.transpose(Image.FLIP_LEFT_RIGHT)
+def ameliorer_qualite_image(chemin_image_entree, chemin_image_sortie, facteur_amelioration):
+    image = Image.open(chemin_image_entree)
+    enhance = ImageEnhance.Contrast(image)
+    image_amelioree = enhance.enhance(facteur_amelioration)
+    image_amelioree.save(chemin_image_sortie)
+    print("Amélioration de la qualité terminée. Image enregistrée à", chemin_image_sortie)
 
-def flip_vertical(img):
-    return img.transpose(Image.FLIP_TOP_BOTTOM)
+def selectionner_image():
+    chemin_image_entree = filedialog.askopenfilename(title="Sélectionner une image", filetypes=[("Images", "*.png;*.jpg;*.jpeg;*.gif")])
 
-def rotate_90(img):
-    return img.transpose(Image.ROTATE_90)
+    if chemin_image_entree:
+        chemin_image_sortie = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("Images PNG", "*.png")])
 
-def rotate_custom(img, degrees):
-    return img.rotate(degrees)
-
-def choose_image():
-    file_path = filedialog.askopenfilename(title="Sélectionnez une image")
-    return file_path
-
-def main():
-    root = tk.Tk()
-    root.withdraw()
-    input_path = choose_image()
-
-    img = Image.open(input_path)
-
-    print("Choisissez une option :")
-    print("1. Flip horizontal")
-    print("2. Flip vertical")
-    print("3. Rotation de 90 degrés")
-    print("4. Rotation personnalisée")
-
-    choice = input("Entrez le numéro de l'option choisie (1, 2, 3 ou 4) : ")
-
-    if choice == '1':
-        img_transformed = flip_horizontal(img)
-    elif choice == '2':
-        img_transformed = flip_vertical(img)
-    elif choice == '3':
-        img_transformed = rotate_90(img)
-    elif choice == '4':
-        degrees = float(input("Entrez l'angle de rotation personnalisé : "))
-        img_transformed = rotate_custom(img, degrees)
-    else:
-        print("Option invalide. Veuillez choisir entre 1, 2, 3 ou 4.")
-        return
-
-    img_transformed.show()
-
-    save_option = input("Voulez-vous sauvegarder l'image transformée? (Oui/Non) : ").lower()
-    if save_option == 'oui':
-        output_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg")])
-        img_transformed.save(output_path)
-        print("Image transformée sauvegardée avec succès.")
+        if chemin_image_sortie:
+            facteur_amelioration = 1.5  # Ajustez ce facteur selon vos besoins
+            ameliorer_qualite_image(chemin_image_entree, chemin_image_sortie, facteur_amelioration)
 
 if __name__ == "__main__":
-    main()
+    root = Tk()
+    root.withdraw()  
+    selectionner_image()
+    root.destroy()
